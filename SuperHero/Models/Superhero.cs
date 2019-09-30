a9261f52c1bb51e3e409dffea453f2aece7c914a
unknown <mmedina8062@gmail.com>
2019-09-29 21:26:44 -0500

    testing

---
+++ SuperHero/Models/Superhero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class Superhero
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string alterEgo { get; set; }
+        public string primarySuperheroAbility { get; set; }
+        public string secondarySuperheroAbility { get; set; }
+        public string catchPhrase { get; set; }
+    }
+}
