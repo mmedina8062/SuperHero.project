7e559f8c8e02bd132c9b6157eee7321a9fd92e21
unknown <mmedina8062@gmail.com>
2019-09-29 21:44:57 -0500

    added public  DbSet<Superhero> Superheroes under Models folder in the identityModel class in the method Application DbContext

--- SuperHero/Models/IdentityModels.cs
+++ SuperHero/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace SuperHero.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public  DbSet<Superhero> Superheroes { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
--- SuperHero/Models/Superhero.cs
+++ SuperHero/Models/Superhero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace SuperHero.Models
 {
     public class Superhero
     {
+        [Key]
         public int Id { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
