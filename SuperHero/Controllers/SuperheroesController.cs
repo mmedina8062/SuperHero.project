81ffcbcbe7f8449468af7ce32955a2188c43c513
unknown <mmedina8062@gmail.com>
2019-09-30 00:13:19 -0500

    created migrations & added Superheroes controller without scaffolding, and instantiated Superhero in Superheroes controller class under ActioResult create method and add logic to ActionResult Create (Superhero superhero) method

---
+++ SuperHero/Controllers/SuperheroesController.cs
@@ -0,0 +1,97 @@
+﻿using SuperHero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHero.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        ApplicationDbContext context;
+        public SuperheroesController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Superheroes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Create
+        public ActionResult Create()
+        {
+            Superhero superhero = new Superhero();
+            return View(superhero);
+        }
+
+        // POST: Superheroes/Create
+        [HttpPost]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Superheroes.Add(superhero);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
